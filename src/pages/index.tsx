60d9f261110e0a9a64be3e37704304dd314c482d
jubitjohn <jubitjohn1@gmail.com>
2023-05-25 13:39:25 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,82 @@
-import React from 'react';
-import { NextPage } from 'next';
-import axios from 'axios';
+// import React from 'react';
+// import { NextPage } from 'next';
+// import axios from 'axios';
+
+// export interface HomeProps {
+//   ipAddress: string;
+//   visitCount: number;
+// }
+
+// const Home: NextPage<HomeProps> = ({ ipAddress, visitCount }) => {
+//   return (
+//     <div>
+//       <h1>Welcome to my Next.js web app!</h1>
+//       <p>Your IP address: {ipAddress}</p>
+//       <p>Count: {visitCount}</p>
+//     </div>
+//   );
+// };
+
+
+// export const getServerSideProps = async () => {
+//   try {
+//     const response = await axios.get('http://ip-api.com/json/');
+//     const ipAddress = response.data.query;
+  
+
+
+//     const visitCountResponse = await fetch('https://peppy-jalebi-6f5a89.netlify.app/api/visit-count');
+//     const visitCountData = await visitCountResponse.json();
+//     const visitCount = visitCountData.visitCount;
+
+
+//     return {
+//       props: {
+//         ipAddress: ipAddress || null,
+//         visitCount: visitCount || null,
+//       },
+//     };
+//   } catch (error) {
+//     console.error('Failed to fetch IP address:', error);
+//     return {
+//       props: {
+//         ipAddress: null,
+//         visitCount: null,
+//       },
+//     };
+//   }
+// };
+
+// export default Home;
+// pages/ssr.tsx
+import { GetServerSideProps, NextPage } from "next";
+import React from "react";
+
 
 export interface HomeProps {
-  ipAddress: string;
+  ip: string;
   visitCount: number;
-}
-
-const Home: NextPage<HomeProps> = ({ ipAddress, visitCount }) => {
-  return (
-    <div>
-      <h1>Welcome to my Next.js web app!</h1>
-      <p>Your IP address: {ipAddress}</p>
-      <p>Count: {visitCount}</p>
-    </div>
-  );
 };
 
+const SSRPage: NextPage<HomeProps> = ({ ip,visitCount}) => {
+  return <div>IP: {ip}</div>;
+};
 
-export const getServerSideProps = async () => {
-  try {
-    const response = await axios.get('http://ip-api.com/json/');
-    const ipAddress = response.data.query;
+export default SSRPage;
 
-    const visitCountResponse = await fetch('https://peppy-jalebi-6f5a89.netlify.app/api/visit-count');
-    const visitCountData = await visitCountResponse.json();
-    const visitCount = visitCountData.visitCount;
-
-
-    return {
-      props: {
-        ipAddress: ipAddress || null,
-        visitCount: visitCount || null,
-      },
-    };
-  } catch (error) {
-    console.error('Failed to fetch IP address:', error);
-    return {
-      props: {
-        ipAddress: null,
-        visitCount: null,
-      },
-    };
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  let ip = req.headers["x-real-ip"];
+  if (!ip) {
+    const forwardedFor = req.headers["x-forwarded-for"];
+    if (Array.isArray(forwardedFor)) {
+      ip = forwardedFor.at(0);
+    } else {
+      ip = forwardedFor?.split(",").at(0) ?? "Unknown";
+    }
   }
+  return {
+    props: {
+      ip,
+    },
+  };
 };
-
-export default Home;
