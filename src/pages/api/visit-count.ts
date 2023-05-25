3f97b73478949d0f30d1fdd80f43550a948adb18
jubitjohn <jubitjohn1@gmail.com>
2023-05-25 23:26:08 +0530

    using server Local storage  instead of memory

--- src/pages/api/visit-count.ts
+++ src/pages/api/visit-count.ts
@@ -1,27 +1,37 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import handler from './hello';
 
-// In-memory storage for visit counts
-const visitCounts: { [ipAddress: string]: number } = {};
+interface VisitCounts {
+  [key: string]: number;
+}
 
 const visitCountHandler = (req: NextApiRequest, res: NextApiResponse) => {
-  // const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress || '';
-  const { ipAddress } = req.query
+  const { ipAddress } = req.query;
+
+  // Get the visit counts from server local storage
+  let visitCounts: VisitCounts = {};
+  if (typeof window === 'undefined') {
+    // Server environment
+    visitCounts = JSON.parse(localStorage.getItem('visitCounts') || '{}');
+  }
 
   // Extract the first IP address if it is an array
   const ipAddressToUse = Array.isArray(ipAddress) ? ipAddress[0] : ipAddress;
-  console.log('IP Address:', ipAddress);
+  console.log('IP Address:', ipAddressToUse);
 
   // Increment visit count for the IP address
   if (ipAddressToUse) {
     visitCounts[ipAddressToUse] = visitCounts[ipAddressToUse] ? visitCounts[ipAddressToUse] + 1 : 1;
   }
 
-  const visitCount = visitCounts[ipAddressToUse||''];
+  // Save the updated visit counts to server local storage
+  if (typeof window === 'undefined') {
+    // Server environment
+    localStorage.setItem('visitCounts', JSON.stringify(visitCounts));
+  }
+
+  const visitCount = visitCounts[ipAddressToUse || ''];
 
   res.status(200).json({ visitCount });
 };
 
 export default visitCountHandler;
-
-
